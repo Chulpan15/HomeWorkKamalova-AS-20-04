97e8ba8d64c4c456930e8b5a459363e151453425
fedorina-irina <90184585+fedorina-irina@users.noreply.github.com>
2021-12-07 21:02:49 +0300

    Бинарный алгоритм

--- TeamHomeWork/TeamHomeWork/Algoritmy.cpp
+++ TeamHomeWork/TeamHomeWork/Algoritmy.cpp
@@ -5,9 +5,26 @@ int& algoritmy::AlgoritmEvklida(int& x, int& y)
 	// TODO: вставьте здесь оператор return
 }
 
-int& algoritmy::BinarniyAlgoritm(int& x, int& y)
+int& algoritmy::BinarniyAlgoritm(int& A, int& B)
 {
-	// TODO: вставьте здесь оператор return
+	int k = 1;
+	while ((A != 0) && (B != 0))
+	{
+		while ((A % 2 == 0) && (B % 2 == 0))
+		{
+			A /= 2;
+			B /= 2;
+			k *= 2;
+		}
+
+		while (A % 2 == 0) A /= 2;
+		while (B % 2 == 0) B /= 2;
+
+		if (A >= B) A -= B;
+		else B -= A;
+	}
+	int result = B * k;
+	return result;
 }
 
 int& algoritmy::RashirenniyAlgoritmEvklida(int& x, int& y)
--- TeamHomeWork/TeamHomeWork/Algoritmy.h
+++ TeamHomeWork/TeamHomeWork/Algoritmy.h
@@ -6,7 +6,7 @@ using namespace std;
 namespace algoritmy
 {
 	int& AlgoritmEvklida(int& x, int& y);
-	int& BinarniyAlgoritm(int& x, int& y);
+	int& BinarniyAlgoritm(int& A, int& B);
 	int& RashirenniyAlgoritmEvklida(int& x, int& y);
 	int& AlgoritmLemmera(int& x, int& y);
 }
