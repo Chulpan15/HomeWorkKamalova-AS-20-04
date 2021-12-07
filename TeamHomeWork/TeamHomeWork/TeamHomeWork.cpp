eb98f185ede0faeda3893f690a331dde3de70481
Chulpan15 <ch_kamalova@mail.ru>
2021-12-07 23:19:33 +0500

    Добавлен Timer

--- TeamHomeWork/TeamHomeWork/TeamHomeWork.cpp
+++ TeamHomeWork/TeamHomeWork/TeamHomeWork.cpp
@@ -5,6 +5,35 @@
 using namespace std;
 using namespace algoritmy;
 
+//Timer
+class Timer
+{
+private:
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration < double, std::ratio <1>>;
+
+	std::chrono::time_point <clock_t> start;
+
+public:
+	Timer() : start(clock_t::now())
+	{
+
+	}
+
+	void reset()
+	{
+		start = clock_t::now();
+	}
+
+	double elapsed() const
+	{
+		return std::chrono::duration_cast <second_t> (clock_t::now() - start).count();
+	}
+
+};
+
+
+
 int main()
 {
 cout << "Enter number A: ";
