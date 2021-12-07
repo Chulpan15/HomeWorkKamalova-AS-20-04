703cec1e4caf6610b796b8258a9fa1c66f562c66
Chulpan15 <ch_kamalova@mail.ru>
2021-12-08 00:30:21 +0500

    Обновление

--- TeamHomeWork/TeamHomeWork/Algoritmy.h
+++ TeamHomeWork/TeamHomeWork/Algoritmy.h
@@ -8,7 +8,7 @@ namespace algoritmy
 	int& AlgoritmEvklida(int& A, int& B);
 	int& BinarniyAlgoritm(int& A, int& B);
 	int& RashirenniyAlgoritmEvklida(int a, int b, int& x, int& y);
-	int& AlgoritmLemmera(int& A, int& B);
+	/*int& AlgoritmLemmera(int& A, int& B);*/
 }
 
 
