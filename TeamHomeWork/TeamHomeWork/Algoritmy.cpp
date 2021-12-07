d7cffe31769417134ccd0a00bcc39036ebdf7e02
LevIvv <leva.ivlev02@gmail.com>
2021-12-07 21:47:17 +0300

    Добавлен расширенный алгоритм Евклида

--- TeamHomeWork/TeamHomeWork/Algoritmy.cpp
+++ TeamHomeWork/TeamHomeWork/Algoritmy.cpp
@@ -10,9 +10,21 @@ int& algoritmy::BinarniyAlgoritm(int& A, int& B)
 	// TODO: вставьте здеcь свой алгоритм
 }
 
-int& algoritmy::RashirenniyAlgoritmEvklida(int& A, int& B)
+int& algoritmy::RashirenniyAlgoritmEvklida(int a, int b, int& x, int& y)
 {
-	// TODO: вставьте здесь свой алгоритм
+	/* a и b - числа, для которых мы ищем НОД, передаем по значению.
+	x и y - переменные, в которые записываются найденные алгоритмом коэффициенты разложения
+	(пустые переменные передаются по ссылке */
+
+	if (a == 0) {
+		x = 0; y = 1;
+		return b;
+	}
+	int x1, y1;
+	int d = RashirenniyAlgoritmEvklida(b % a, a, x1, y1);
+	x = y1 - (b / a) * x1;
+	y = x1;
+	return d;
 }
 
 int& algoritmy::AlgoritmLemmera(int& A, int& B)
