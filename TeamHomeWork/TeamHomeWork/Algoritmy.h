1f0eb0b8b5ab3614cf6bb14ae3df6fc39e59559a
mikhail-ross <89380584+mikhail-ross@users.noreply.github.com>
2021-12-12 20:35:00 +0300

    Update Algoritmy.h

--- TeamHomeWork/TeamHomeWork/Algoritmy.h
+++ TeamHomeWork/TeamHomeWork/Algoritmy.h
@@ -10,7 +10,7 @@ namespace algoritmy
 	int& AlgoritmEvklida(int& A, int& B);
 	int& BinarniyAlgoritm(int& A, int& B);
 	int& RashirenniyAlgoritmEvklida(int a, int b, int& x, int& y);
-	int& AlgoritmLemmera(int& A, int& B);
+	int& AlgoritmLemmera(string& pA, string& pB, int &base);
 }
 
 
